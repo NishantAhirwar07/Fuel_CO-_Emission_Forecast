13d7fee799c867cad808344f9c2c25cf6bbb7a07
NishantAhirwar07 <nishantahirwar012@gmail.com>
2026-05-05 17:01:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,13 +147,10 @@ BRAND_HINTS = {
 # Header
 st.markdown("""
 <div style="text-align:center;padding:30px 0 18px">
-    <div style="font-size:2.8rem"></div>
-    <h1 style="font-size:1.85rem;font-weight:800;color:#f3f4f6;margin:10px 0 6px">
+    <div style="font-size:1.8rem"></div>
+    <h1 style="font-size:1.0rem;font-weight:800;color:#f3f4f6;margin:10px 0 6px">
         CO₂ Emission Predictor
     </h1>
-    <p style="color:#6b7280;font-size:0.92rem;margin:0">
-        Enter your vehicle details below to predict carbon emissions
-    </p>
 </div>
 """, unsafe_allow_html=True)
 
