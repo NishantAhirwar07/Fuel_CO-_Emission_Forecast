8e282246046ef54d910bb0fdaf73fc2f08f11f6e
NishantAhirwar07 <nishantahirwar012@gmail.com>
2026-05-05 14:30:21 +0530

    Remove unused import for plotly.express

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import plotly.express as px
 import time
 
 # ── Page Config ─────────────────────────────────────────────────────────────
